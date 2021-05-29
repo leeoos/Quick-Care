6a10061f81330d212a51d66556d38c2424378129
leeoos <leeoos@pop-os.localdomain>
2021-05-29 12:10:07 +0200

    readme

--- WebPage/Area_Riservata/index.php
+++ WebPage/Area_Riservata/index.php
@@ -1,6 +1,6 @@
 <?php
     // Indice di indirizzamento all'interno dell'Area Riservata
-    session_start();
+    @session_start();
     
     // se la sessione e' attiva consente l'accesso automatico 
     // all'Area Riservata importando la pagina area-riservata.php
